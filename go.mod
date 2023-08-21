4defa9f134d98d0c3b8e8fc5136d622e0732f326
quartzwine <quartzwine@gmail.com>
2023-08-20 19:21:17 -0700

    moving unit test to main and adding profiler

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.17
 require github.com/n0madic/twitter-scraper v0.0.0-20230711213008-94503a2bc36c
 
 require (
+	github.com/felixge/fgprof v0.9.3 // indirect
+	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/pkg/profile v1.7.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/quartzwine/friend-tech-utils/db"
+	"github.com/quartzwine/friend-tech-utils/twitter"
+    "github.com/pkg/profile"
+
+	twitterscraper "github.com/n0madic/twitter-scraper"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    scraper := twitterscraper.New()
+    addresses := db.Get_addresses()
+
+    defer profile.Start(profile.CPUProfile).Stop()
+
+    //fmt.Println("Addresses:")
+    for _, address := range addresses {
+        username, err := twitter.GetTwitterUsernameFromAddress(address)
+        if (err != nil) {
+            panic(err)
+        }
+        profile, err := scraper.GetProfile(username)
+        if (err != nil) {
+            fmt.Printf("couldnt find username: %s\n", username)
+            continue
+        }
+        if (profile.FollowersCount > 1000) {
+            fmt.Printf("Address: %s Username: %s, Followers: %d\n", address, username, profile.FollowersCount)
+        } else{
+            fmt.Print("skipping user under 1k followers\n")
+        }
+        
+    }
+    
 }
